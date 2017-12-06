f167fe3eb7ed6eae8aa96a5e08f3f8bfcc90ea13
elisei1000 <elisei856@gmail.com>
2017-12-06 10:25:47 +0200

    Added for title and description getters and setters

--- src/main/java/com/gymwebapp/domain/Course.java
+++ src/main/java/com/gymwebapp/domain/Course.java
@@ -36,6 +36,22 @@ public class Course implements HasId<Integer>{
     @Column(name="title")
     private String title;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Column(name="description")
     private String description;
 
