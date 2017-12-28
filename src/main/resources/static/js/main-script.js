5c6f72a9c350b0ac5849d468afe1bc8b00561431
diana <diana.gociu@yahoo.com>
2017-12-28 15:19:56 +0200

    design for schedule page

--- src/main/resources/static/js/main-script.js
+++ src/main/resources/static/js/main-script.js
@@ -28,3 +28,15 @@ function submitLogin() {
         }
     });
 }
+$(function() {
+    $('form').each(function() {
+        $(this).find('input').keypress(function(e) {
+            // Enter pressed?
+            if(e.which == 10 || e.which == 13) {
+                submitLogin();
+            }
+        });
+
+        $(this).find('input[type=submit]').hide();
+    });
+});
