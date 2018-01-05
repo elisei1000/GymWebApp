3d6ecfbd9c5a44660a9220995ec9e00b49c3aded
diana <diana.gociu@yahoo.com>
2018-01-06 00:28:20 +0200

    show schedule

--- src/main/resources/static/js/schedule-script.js
+++ src/main/resources/static/js/schedule-script.js
@@ -2,16 +2,60 @@
  * Created by diana on 29.12.2017.
  */
 
-var program=[{id:1,difficultyLevel:0,startHour: new Date("2017/12/13 7:00 AM"),endHour:new Date("2017/12/13 7:00 PM"),startDate:"2017/12/12",endDate:"2017/12/20",maxPlaces:10 },]
-$(function() {
-    $("#scheduler").kendoScheduler({
-    date: new Date(), // The current date of the scheduler
-        dataSource: [
-            {
-                id: program[0].id, // Unique identifier.
-                start: program[0].startHour, // Start of the event
-                end: program[0].endHour, // End of the event
-                title: "Course "+program[0].id // Title of the event
-            },
-        ]
-    });});
+
+DevExpress.viz.currentTheme("generic.light");
+
+$(function(){
+
+    $("#scheduler").dxScheduler({
+        dataSource: data,
+        views: ["day","week", "month"],
+        currentView: "week",
+        currentDate: new Date(),
+        startDayHour: 9,
+        height: 600,
+        onCellClick: function(e) {
+            e.cancel = true;
+        },
+        onCelDblClick: function(e) {
+            e.cancel = true;
+        },
+        onAppointmentClick: function(e) {
+            e.cancel = true;
+        },
+        onAppointmentDblClick: function(e) {
+            e.cancel = true;
+        }
+    }).dxScheduler("instance");
+
+});
+
+/*var data = $.ajax({
+    url: "/course",
+    async: false,
+    dataType: 'json'
+}).responseJSON;
+console.log(data);
+*/
+var data =[
+    {
+        "id": 1,
+        "difficultyLevel": 0,
+        "startHour": "09:00:00+01:00",
+        "endHour": "10:00:00+01:00",
+        "startDate": "2018-01-04T09:00:00+01:00",
+        "endDate": "2018-01-04T10:00:00+01:00",
+        "maxPlaces": 10,
+        "text": "Curs1"
+    },
+    {
+        "id": 2,
+        "difficultyLevel": 0,
+        "startHour": "09:00:00+01:00",
+        "endHour": "10:00:00+01:00",
+        "startDate": "2018-01-05T09:00:00+01:00",
+        "endDate": "2018-01-05T10:00:00+01:00",
+        "maxPlaces": 10,
+        "text": "Curs2"
+    }
+];
